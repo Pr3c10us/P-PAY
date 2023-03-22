08d203c52fdd2220668aa2da940b784b206cde8d
Pr3c10us <owo.pre.eno@gmail.com>
2023-03-22 23:18:27 +0100

    dark-mode issue

--- client/app/layout.js
+++ client/app/layout.js
@@ -2,13 +2,13 @@ import './globals.css';
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en" className="h-full scroll-smooth">
+        <html lang="en" className="h-full scroll-smooth dark:text-black">
             {/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
             <head />
-            <body className="h-full scroll-smooth bg-neutral font-bai">
+            <body className="h-full scroll-smooth bg-neutral font-bai dark:text-black">
                 {children}
             </body>
         </html>

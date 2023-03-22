bb59d7d166708ae310bbf0dd733df050ad42c14a
Pr3c10us <owo.pre.eno@gmail.com>
2023-03-23 00:33:58 +0100

    removed top borders

--- client/app/dashboard/transfer/formOne.js
+++ client/app/dashboard/transfer/formOne.js
@@ -51,7 +51,7 @@ const FormOne = ({ setName, setForm, setUsername, user }) => {
     return (
         <form
             autoComplete="off"
-            className="flex w-full max-w-xl flex-col space-y-2 place-self-center rounded-xl border border-t-4 border-t-secondary bg-neutral px-2 pt-4 pb-4 shadow-md sm:justify-center sm:space-y-4 sm:px-8 sm:pt-8"
+            className=" flex w-full max-w-xl flex-col space-y-2 place-self-center rounded-xl border border-t-4 bg-neutral px-2 pt-4 pb-4 shadow-md sm:justify-center sm:space-y-4 sm:px-8 sm:pt-8"
             onSubmit={formik.handleSubmit}
         >
             <h1 className="text-2xl font-medium sm:text-3xl">Send Money</h1>
--- client/app/dashboard/withdraw/success.js
+++ client/app/dashboard/withdraw/success.js
@@ -5,7 +5,7 @@ import { BsCheckLg } from 'react-icons/bs';
 const Success = () => {
     const router = useRouter();
     return (
-        <div className="flex bg-neutral border-t-4 border-t-secondary w-full max-w-[20rem] flex-col items-center space-y-14 place-self-center rounded-xl border px-2 py-10 font-medium shadow-md xsm:justify-center xsm:space-y-14 xsm:py-10 xsm:px-8 sm:max-w-sm">
+        <div className=" flex w-full max-w-[20rem] flex-col items-center space-y-14 place-self-center rounded-xl border border-t-4 bg-neutral px-2 py-10 font-medium shadow-md xsm:justify-center xsm:space-y-14 xsm:py-10 xsm:px-8 sm:max-w-sm">
             <div className="flex h-24 w-24 items-center justify-center rounded-full bg-emerald-400 xsm:h-24 xsm:w-24 ">
                 <div className="absolute flex h-40 w-40 animate-wiggle items-center justify-center rounded-full bg-emerald-400 bg-opacity-25 xsm:h-40 xsm:w-40">
                     <div className="flex h-32 w-32 animate-wiggle items-center justify-center rounded-full bg-emerald-400 bg-opacity-40 xsm:h-32 xsm:w-32"></div>

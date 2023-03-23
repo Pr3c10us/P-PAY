b626cb4ae643c760b7b63bd4d56fd0922010fbbe
Pr3c10us <owo.pre.eno@gmail.com>
2023-03-23 17:21:22 +0100

    fixed router issue

--- client/app/dashboard/home/options.js
+++ client/app/dashboard/home/options.js
@@ -1,50 +1,49 @@
 'use client';
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { CgHomeAlt } from 'react-icons/cg';
+
 import { AiFillDollarCircle } from 'react-icons/ai';
 import { FaUserCircle } from 'react-icons/fa';
-import { IoLogOut, IoReceipt } from 'react-icons/io5';
+import { IoReceipt } from 'react-icons/io5';
 import { usePathname, useRouter } from 'next/navigation';
-import axios from 'axios';
 
 const Options = () => {
+    const router = useRouter();
     return (
         <ul
-            className={`mt-4 grid w-full flex-1 grid-cols-3 flex-col gap-x-4 sm:gap-x-8 gap-y-4 px-4 sm:grid-cols-3 md:hidden`}
+            className={`mt-4 grid w-full flex-1 grid-cols-3 flex-col gap-x-4 gap-y-4 px-4 sm:grid-cols-3 sm:gap-x-8 md:hidden`}
         >
-            <div className='flex items-center justify-center w-full h-full'>
+            <div className="flex h-full w-full items-center justify-center">
                 <li
                     onClick={() => {
                         router.push('/dashboard/transfer');
                     }}
-                    className={` flex w-full cursor-pointer gap-y-1 items-center justify-center gap-x-2 flex-col py-2 px-4 text-base`}
+                    className={` flex w-full cursor-pointer flex-col items-center justify-center gap-y-1 gap-x-2 py-2 px-4 text-base`}
                 >
-                    <AiFillDollarCircle className="rounded-2xl border-2 p-1 text-secondary border-secondary w-8 h-8" />
-                    <p className="text-sm xsm:text-lg">Send Money</p>
+                    <AiFillDollarCircle className="h-8 w-8 rounded-2xl border-2 border-secondary p-1 text-secondary" />
+                    <p className="text-sm xsm:text-lg">Transfer</p>
                 </li>
             </div>
-            <div className='flex items-center justify-center w-full h-full'>
+            <div className="flex h-full w-full items-center justify-center">
                 <li
                     onClick={() => {
                         router.push('/dashboard/transactions');
                     }}
-                    className={`flex w-full cursor-pointer gap-y-1 items-center justify-center gap-x-2 flex-col py-2 px-4 text-base `}
+                    className={`flex w-full cursor-pointer flex-col items-center justify-center gap-y-1 gap-x-2 py-2 px-4 text-base `}
                 >
-                    <IoReceipt className="rounded-2xl border-2 p-1 text-secondary border-secondary w-8 h-8" />
-                    <p className="2xsm:text-sm text-xs xsm:text-lg">Transactions</p>
+                    <IoReceipt className="h-8 w-8 rounded-2xl border-2 border-secondary p-1 text-secondary" />
+                    <p className="text-xs 2xsm:text-sm xsm:text-lg">
+                        Transactions
+                    </p>
                     {/* IoReceipt */}
                 </li>
             </div>
-            <div className='flex items-center sm:col-span-1 justify-center w-full h-full'>
+            <div className="flex h-full w-full items-center justify-center sm:col-span-1">
                 <li
                     onClick={() => {
                         router.push('/dashboard/profile');
                     }}
-                    className={`flex w-min sm:w-full cursor-pointer gap-y-1 items-center justify-center gap-x-2 flex-col py-2 px-4 text-base`}
+                    className={`flex w-min cursor-pointer flex-col items-center justify-center gap-y-1 gap-x-2 py-2 px-4 text-base sm:w-full`}
                 >
-                    <FaUserCircle className="rounded-2xl border-2 p-1 text-secondary border-secondary w-8 h-8" />
+                    <FaUserCircle className="h-8 w-8 rounded-2xl border-2 border-secondary p-1 text-secondary" />
                     <p className="text-sm xsm:text-lg">Profile</p>
                 </li>
             </div>
